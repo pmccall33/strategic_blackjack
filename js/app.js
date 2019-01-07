6fbf7d0ca67fd23df5c60479b150a8fa57af6c66
pmccall <pmccall33@gmail.com>
2019-01-07 12:00:42 -0600

    Card class created

--- js/app.js
+++ js/app.js
@@ -1,15 +1,7 @@
 console.log('Howdy- Blackjack');
 
 
-// create object deck
-// const deck = [
-// 	{
-// 	card: 'A - hearts',
-// 	cardNumValue: 11, 
-// 	cardImage: null,	
-// 	}
-// ]
-		
+
 	// let deck = {}
 
 	// // Suits - Hearts, Spades, Diamonds, Clubs
@@ -29,6 +21,17 @@ console.log('Howdy- Blackjack');
 
 // Create Deck - Class  - array of objects?
 
+class Card {
+	constructor(rank, suit, value) {
+		this.rank = rank;
+		this.suit = suit;
+		this.value = value;
+		// this.image = image;
+	}
+}
+const newCard = new Card('Ace', 'hearts', 11);
+console.log(newCard);
+
 class DeckOfCards {	
 	
 	constructor(card) {
@@ -41,51 +44,68 @@ class DeckOfCards {
 		const suits = ['hearts', 'spades', 'diamonds', 'clubs'];
 		// Cards - A - K  - Array?
 		const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+		// Card values
+		const values = [11, 2 ,3 , 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
 
 		for (let suit in suits) {
 	      for (let card in cards) {
-	        this.deck.push(`${cards[card]} - ${suits[suit]}`);
+	      	//new Card
+	        const newCard = new Card(cards[card], suits[suit]);
+	        this.deck.push(newCard);
+	        // this.deck.push(`${cards[card]} - ${suits[suit]}`);		      
 	      }
 	    }
-
+	    
+	    for (let i = 0; i < (this.deck.length/ 4); i++) {
+	    	this.deck[i].value = values[i];
+	    }
+	    for (let i = 13; i < (this.deck.length/ 2); i++) {
+	    	this.deck[i].value = values[i - 13];
+	    }
+	    for (let i = 26; i < (this.deck.length * .75); i++) {
+	    	this.deck[i].value = values[i - 26];
+	    }
+	    for (let i = 39; i < (this.deck.length); i++) {
+	    	this.deck[i].value = values[i - 39];
+	    }
 	}	
-	
-
+ 
 	// Get Cards numeric value
 
-		getNumValue(card) {
-			 // this.cardNumValue = 0;
-			 for (let i = 0; i < this.deck.length; i++) {
-			 	if (this.deck[i][0] === 'J' ||
-			 		this.deck[i][0] === 'Q' ||
-			 		this.deck[i][0] === 'K' ||
-			 		this.deck[i][0] === '1') {
-			 			this.cardNumValue = 10;
-			 		// console.log(this.cardValue);
-			 	} else if (this.deck[i][0] === 'A') {
-			 		this.cardNumValue = 11;
-			 	} else {
-			 		this.cardNumValue = this.deck[i][0];
-			 	}
-			 	// console.log(this.cardNumValue);
-			 }
-			 return this.cardNumValue;
+	getNumValue(card) {
+		console.log(card)
+		 // this.cardNumValue = 0;
+		for (let i = 0; i < this.deck.length; i++) {
+			// console.log(this.deck[i])=
+			if (this.deck[i][0] === 'J' ||
+				this.deck[i][0] === 'Q' ||
+				this.deck[i][0] === 'K' ||
+				this.deck[i][0] === '1') {
+					// console.log("face card or ace ten")
+					this.cardNumValue = 10;
+				// console.log(this.cardValue);
+			} else if (this.deck[i][0] === 'A') {
+				// console.log("A")
+				this.cardNumValue = 11;
+			} else {
+				// console.log("not an ace or face card")
+				this.cardNumValue = this.deck[i][0];
+			}
+			// console.log(this.cardNumValue);
 		}
+		return this.cardNumValue;
+	}
 	
-
-
-
-	// Shuffle cards
 };
 
 const newDeck = new DeckOfCards();
-
 console.log(newDeck.deck);
-console.log(newDeck.deck.length);
-console.log(newDeck.deck[0], newDeck.deck[13], newDeck.deck[26], newDeck.deck[39]);
-console.log(newDeck.deck[1][0]);
-console.log(newDeck.getNumValue(newDeck.deck[4]));
-
+// console.log(newDeck.deck.length);
+// console.log(newDeck.deck[0], newDeck.deck[13], newDeck.deck[26], newDeck.deck[39]);
+// console.log(newDeck)
+// console.log(newDeck.deck[5][0]);
+// console.log("here it is", newDeck.getNumValue(newDeck.deck[5]));
+// console.log(newDeck.deck[0]);
 
 // Create PLayer Class 
 
@@ -124,20 +144,27 @@ class Dealer extends Player {
 
 }
 
-
+// const randomIndex = function(arr) {
+// 	let rndmIdx = Math.floor(Math.random() * arr.length);
+//  	return rndmIdx;
+// }
 // Create Game Object 
 
 const game = {
 	gameOn: false,
 	deck: null,
 	shuffledDeck: [],
-	numOfPlayers: 0,
+	numOfPlayers: 2,
 	dealerHand: [],
 	dealerHandTotal: 0,
-	playerHand: [],
-	playerHandTotal: 0,
+	playerOneHand: [],
+	playerOneHandTotal: 0,
+	playerTwoHand: [],
+	playerTwoHandTotal: 0,
 	currentPlayer: null,
-
+	playerOne: null,
+	playerTwo: null,
+	playerHasAce: false,
 
 
 	shuffleCards () {
@@ -145,6 +172,7 @@ const game = {
 		let randomIndex = Math.floor(Math.random() * newDeck.deck.length);
 		console.log(randomIndex);
 		// console.log(newDeck.deck);
+		
 		// TODO: why it takes many iterations
 		for (let i = 0; i < (newDeck.deck.length * 100); i++) {
 			this.shuffledDeck.push(newDeck.deck.splice(Math.floor(Math.random() * newDeck.deck.length), 1));
@@ -152,12 +180,36 @@ const game = {
 		console.log(this.shuffledDeck);
 	},
 
+	togglePlayer() {
+		if (playerOne) {
+			this.currentPlayer = playerTwo;
+		} else {
+			this.currentPlayer = playerOne;
+		}
+		console.log(currentPlayer);
+	},
+
 	startDeal () {
-		
+			//deal 2 cards to player
+			this.dealCard();
+			this.dealCard();
+			console.log(this.playerOneHand);
+			return this.playerOneHand;
+
 	},
 
 	checkForAce() {
-		
+		console.log(this.playerOneHand[0].rank);
+		if ((this.playerOneHand.includes('A - hearts')) ||
+			(this.playerOneHand.includes('A - spades')) ||
+			(this.playerOneHand.includes('A - diamonds')) ||
+			(this.playerOneHand.includes('A - clubs'))) {
+				this.playerHasAce = true;
+			} else {
+				this.playerHasAce = false;
+			}
+			console.log(this.playerHasAce);
+			return this.playerHasAce;
 	},
 	
 	checkForBlackjack() {
@@ -166,9 +218,9 @@ const game = {
 
 	dealCard() {
 		//add a card to playerHand from shuffleDeck
-		this.playerHand.push(this.shuffledDeck.pop());
-		console.log(this.playerHand);
-		console.log(this.shuffledDeck);
+		this.playerOneHand.push(this.shuffledDeck.pop());
+		// console.log(this.playerOneHand);
+		// console.log(this.shuffledDeck);
 	},
 
 	checkForBust() {
@@ -183,13 +235,18 @@ const game = {
 }
 
 game.shuffleCards();
-game.dealCard();
+// game.dealCard();
+game.startDeal();
+game.checkForAce();
+// console.log(game.playerOneHand.includes('A - hearts'));
+// console.log(game.playerOneHand.includes('A - spades'));
+// console.log(game.playerOneHand.includes('A - diamonds'));
+// console.log(game.playerOneHand.includes('A - clubs'));
+// console.log(true);
+// console.log(newDeck.deck[0]);
 
 
-
-
-
-
+	
 
 
 
