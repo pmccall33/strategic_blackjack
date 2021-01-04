b967cc5da8869c305f0a8fecab5deee0fd57f00a
pmccall <pmccall33@gmail.com>
2021-01-04 02:30:32 -0600

    continue convert testing and card-images to .mjs and update package.json include type=module

--- js/HelperFunctions.mjs
+++ js/HelperFunctions.mjs
@@ -8,27 +8,27 @@ class HelperFunctions {
 	createEnumFromArr(Arr) {
 		let newEnum = new Object();
 		Arr.forEach((e) =>  {
-			newEnum[e.toUpperCase()] = Symbol(`${e}`);
+			typeof(e) === 'string' ? newEnum[e.toUpperCase()] = Symbol(`${e}`) : newEnum[e] = Symbol(`${e}`);
 		});
 		// console.log(newEnum);
 		return Object.freeze(newEnum);
 	}
 
 	// Takes in an object and returns an enum-like immutable obj of the 
-	// structure { KEY : Symbol(value)}
-	createEnumFromObj(Obj) {
+	// structure { KEY : Symbol(value)} 
+	createEnumFromObjOfStrings(Obj) {
 		let newEnum = new Object;
 		for (const prop in Obj) {
 			newEnum[`${prop}`.toUpperCase()] = Symbol(`${Obj[prop]}`);
 		};
-		console.log(newEnum);
+		// console.log(newEnum);
 		return Object.freeze(newEnum);
 	}
 };
 
-// const HelperFunctionsInstance = new HelperFunctions;
-// const shmoo = HelperFunctionsInstance.createEnumFromArr(['Dog', 'Cat', 'Aardvark']);
-// // shmoo.DOG = '789253'; 
+const HelperFunctionsInstance = new HelperFunctions;
+const shmoo = HelperFunctionsInstance.createEnumFromArr(['Dog', 12, 'Cat', 'Aardvark']);
+// shmoo.DOG = '789253'; 
 // console.log(shmoo);
 // const schmoo2 = HelperFunctionsInstance.createEnumFromObj({'dog': '🐶', 'koala': '🐨', 'alien': '👽'});
 // console.log(schmoo2);
